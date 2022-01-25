62e5c501ee57d2e074257a2cd5f7b47cac309f8d
Andrew McNeill <andrew@andrewmc.ca>
2022-01-24 22:55:06 -0400

    Completed functionality, added a test

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,14 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+  it("should false if the word or letters array is empty", function() {
+    const result = wordSearch([], '');
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (!word || word.length === 0 || !letters || letters.length === 0) {
+    return false;
+  }
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  // this ^ is a string
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let element = '';
+    for (let j = 0; j < letters.length; j++) {
+      element += letters[j][i];
+    }
+    verticalJoin.push(element);
+  }
+  // we want first index of each array, then second
+  
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+
+};
+const letters = [];
+const word = '';
+console.log(wordSearch(letters, word));
+
+module.exports = wordSearch;
